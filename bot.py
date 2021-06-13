25d936cf5cd8f8445be3d74e3856e9d568507ab4
Complexor <jozbornn@gmail.com>
2021-06-13 13:50:49 -0700

    Update bot.py
    Relocated logger, import initialize_il_commands

--- bot.py
+++ bot.py
@@ -5,60 +5,20 @@ from discord.ext import commands
 
 # Math functions, the core of IndieBot
 from indie_math import indie_seq, indie_oeis, indie_collatz, indie_pig
+from indie_library.il_commands import initialize_il_commands
+from indie_utils import logger
 
 import indie_help
 
-mods = []
 oeis_in_progress = False
 
 
-def logger(command_type):
-    # Encapsulates bot commands for added functionality
-    def command_wrapper(command):
-
-        # Where the magic happens. This gives commands more versatility
-        async def function_wrapper(ctx):
-
-            def has_permission():
-                global mods
-                if command_type == "modonly" and str(ctx.message.author.id) in mods:
-                    return True
-                elif command_type == "all":
-                    return True
-                elif command_type == "pig-math":
-                    return ctx.message.channel.name == "pig-math" or ctx.message.channel.name == "bot-testing"
-                else:
-                    return False
-
-            args = ctx.message.content.split(" ")[1:]
-            print("Command used:", command.__name__)
-            try:
-                if has_permission():
-                    await command(ctx, *args)
-                    # Log successful command usage
-                    with open("dat/command_log.csv", "a+") as f:
-                        f.write(f"{command.__name__},{ctx.message.author.id}\n")
-                else:
-                    await ctx.message.add_reaction("❌")
-                    print(
-                        f"User {ctx.message.author.name} does not have permission to use command {command.__name__}")
-            except Exception:
-                await ctx.message.add_reaction("❌")
-                print(traceback.format_exc())
-
-        return function_wrapper
-
-    return command_wrapper
-
-
 def create_bot_instance(prefix, bot_id):
-    global mods
     inst = commands.Bot(command_prefix=prefix)
-    with open("dat/mods.csv", "r+") as f:
-        for each in f.readlines():
-            mods.append(each.replace("\n", ""))
+
     initialize_events(inst)
     initialize_commands(inst)
+    initialize_il_commands(inst)
 
     inst.remove_command("help")
     initialize_help_commands(inst)
